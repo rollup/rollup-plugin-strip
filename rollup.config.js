38f84b8326f1198b6c8f11320b14d023e9732d1d
Brian Donovan <me@brian-donovan.com>
2016-06-10 06:37:30 -0700

    Consolidate package.json build scripts as rollup targets.

--- rollup.config.js
+++ rollup.config.js
@@ -2,5 +2,15 @@ import buble from 'rollup-plugin-buble';
 
 export default {
 	entry: 'src/index.js',
-	plugins: [ buble() ]
+	plugins: [ buble() ],
+	targets: [
+		{
+			format: 'cjs',
+			dest: 'dist/rollup-plugin-strip.cjs.js'
+		},
+		{
+			format: 'es6',
+			dest: 'dist/rollup-plugin-strip.es.js'
+		}
+	]
 };
