f563d92e48e19fd04b8b3c034369d92d32f45b72
Rob Huehn <rhuehn@gmail.com>
2018-02-06 13:36:36 -0800

    fix more rollup option name changes

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ import buble from 'rollup-plugin-buble';
 export default {
 	input: 'src/index.js',
 	plugins: [ buble() ],
-	targets: [
+	output: [
 		{
 			format: 'cjs',
-			dest: 'dist/rollup-plugin-strip.cjs.js'
+			file: 'dist/rollup-plugin-strip.cjs.js'
 		},
 		{
 			format: 'es',
-			dest: 'dist/rollup-plugin-strip.es.js'
+			file: 'dist/rollup-plugin-strip.es.js'
 		}
 	]
 };
